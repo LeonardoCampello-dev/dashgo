83e88e0555f43ca6ad15e880ca3cc95512fe9269
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-07-03 23:02:10 -0300

    💥 NEW: Sign in page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,34 @@
+import { Flex, Button, Stack } from '@chakra-ui/react';
+
+import { Input } from '../components/Form/Input';
+
 export default function Home() {
-  return <h1>Dashgo</h1>;
+  return (
+    <Flex
+      width='100vw'
+      height='100vh'
+      align='center'
+      justify='center'
+    >
+      <Flex
+        as='form'
+        width='100%'
+        maxWidth={360}
+        padding={8}
+        bg='gray.800'
+        borderRadius={8}
+        flexDir='column'
+      >
+        <Stack spacing={4}>
+          <Input name='email' type='email' label='E-mail' />
+
+          <Input name='password' type='password' label='Senha' />
+        </Stack>
+
+        <Button type='submit' mt={6} colorScheme='pink' size='lg'>
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
